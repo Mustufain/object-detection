d5fb8f31f9cd8485b92da04ba045f29f6ed29b28
mustufain.abbas <mustufain.abbas@caidio.io>
2019-11-26 16:05:41 +0200

    Restore file contents that were previously in LFS

--- tf_serving.py
+++ tf_serving.py
@@ -14,59 +14,7 @@ import base64
 import os
 import json
 import requests
-
-
-def iou(box1, box2):
-    """Implement the intersection over union (IoU) between box1 and box2
-        
-        Arguments:
-        box1 -- first box, numpy array with coordinates (x1, y1, x2, y2)
-        box2 -- second box, numpy array with coordinates (x1, y1, x2, y2)
-        """
-
-    xi1 = max(box1[0], box2[0])
-    yi1 = max(box2[1], box2[1])
-    xi2 = min(box1[2], box2[2])
-    yi2 = min(box1[3], box2[3])
-    inter_area = max(((xi2 - xi1) * (yi2 - yi1)), 0)
-
-    # Calculate the Union area by using Formula: Union(A,B) = A + B - Inter(A,B)
-    box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
-    box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
-    union_area = box1_area + box2_area - inter_area
-
-    # compute the IoU
-    iou = inter_area / union_area
-    return iou
-
-
-def filter_boxes(scores, threshold = 0.6):
-    """Filters YOLO boxes by thresholding on object and class confidence.
-
-        """
-    index = np.where(scores > threshold)
-    return index
-
-
-def non_max_suppression(boxes, scores, classes, iou_threshold=0.5):
-    """
-        Applies Non-max suppression (NMS) to set of boxes.
-
-        """
-    box_with_high_iou = boxes[0]
-    # Compute its overlap with all other boxes, and remove boxes that overlap it more than iou_threshold.
-    for box in boxes: # descending order of scores
-        box_selected = box
-        for index, box in enumerate(boxes):
-            iou_score = iou(box_selected, box)
-            if iou_score > iou_threshold:
-                # remove box
-                exclude_boxes.append(index)
-
-    return
-
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = ROOT_DIR + '/workspace/training_demo/exported_graphs/frozen_inference_graph.pb'
+import time
 
 # List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = ROOT_DIR + '/workspace/training_demo/data/label_map.pbtxt'
@@ -74,62 +22,190 @@ PATH_TO_LABELS = ROOT_DIR + '/workspace/training_demo/data/label_map.pbtxt'
 # Number of classes to detect
 NUM_CLASSES = 1
 
-# Load a (frozen) Tensorflow model into memory.
-detection_graph = tf.Graph()
-with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-        serialized_graph = fid.read()
-        od_graph_def.ParseFromString(serialized_graph)
-        tf.import_graph_def(od_graph_def, name='')
-
 
 # Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+# Label maps map indices to category names, so that when our convolution network predicts `1`, we know that this corresponds
+# to `hand`.  Here we use internal utility functions,
+# but anything that returns a dictionary mapping integers to appropriate string labels would be fine
+
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(
     label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
 
+def prepare_request(image):
+    with tf.gfile.GFile(image, 'rb') as fid:
+        encoded_input_string = base64.b64encode(fid.read())
+    input = cv2.imread(image)
+    input_string = encoded_input_string.decode("utf-8")
+    instance = [{'image_bytes': {"b64": input_string}}]
+    data = json.dumps({"instances": instance})
+    return [input, data]
 
-with tf.gfile.GFile(('test_image/hand1.jpg'), 'rb') as fid:
-    encoded_input_string = base64.b64encode(fid.read())
-input = cv2.imread('test_image/hand1.jpg')
-input_string = encoded_input_string.decode("utf-8")
-instance = [{'image_bytes': {"b64": input_string}}]
-data = json.dumps({"instances": instance})
-url = "http://localhost:8501/v1/models/faster_rcnn:predict"
-json_response = requests.post(url=url, data=data)
-response = json.loads(json_response.text)
-boxes = response['predictions'][0]['detection_boxes']
-classes = response['predictions'][0]['detection_classes']
-scores = response['predictions'][0]['detection_scores']
-scores = np.squeeze(scores)
-boxes = np.squeeze(boxes)
-classes = np.squeeze(classes).astype(np.int32)
 
-print (scores)
-exit(1)
-index = filter_boxes(scores)
-boxes = boxes[index]
-scores = scores[index]
-classes = classes[index]
-print (boxes)
-print (scores)
+def get_iou(box1, box2):
+    """Implement the intersection over union (IoU) between box1 and box2
+        
+        Arguments:
+        box1 -- first box, numpy array with coordinates (ymin, xmin, ymax, xmax)
+        box2 -- second box, numpy array with coordinates (ymin, xmin, ymax, xmax)
+        """
+    # ymin, xmin, ymax, xmax = box
+    print (box1, box2)
+    y11, x11, y21, x21 = box1
+    y12, x12, y22, x22 = box2
 
-#boxes, scores, classes = non_max_suppression(boxes, scores, classes)
-#print (boxes)
-#print (scores)
-#print (classes)
+    yi1 = max(y11, y12)
+    xi1 = max(x11, x12)
+    yi2 = min(y21, y22)
+    xi2 = min(x21, x22)
+    inter_area = max(((xi2 - xi1) * (yi2 - yi1)), 0)
+    # Calculate the Union area by using Formula: Union(A,B) = A + B - Inter(A,B)
+    box1_area = (x21 - x11) * (y21 - y11)
+    box2_area = (x22 - x12) * (y22 - y12)
+    union_area = box1_area + box2_area - inter_area
+    # compute the IoU
+    iou = inter_area / union_area
+    return iou
+
+
+def non_max_suppression(boxes, probs, overlapthresh=0.5):
+    """
+        Applies Non-max suppression (NMS) to set of boxes.
+
+        """
+
+    if len(boxes) == 0:
+        return []
+        # initialize the list of picked indexes
+    pick = []
+
+    # grab the coordinates of the bounding boxes
+    x1 = boxes[:, 1]
+    y1 = boxes[:, 0]
+    x2 = boxes[:, 3]
+    y2 = boxes[:, 2]
+
+    # compute the area of the bounding boxes and grab the indexes to sort
+    # (in the case that no probabilities are provided, simply sort on the
+    # bottom-left y-coordinate)
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = y2
+
+    # if probabilities are provided, sort on them instead
+    if probs is not None:
+        idxs = probs
+
+    # sort the indexes
+    idxs = np.argsort(idxs)
+
+    # keep looping while some indexes still remain in the indexes list
+    while len(idxs) > 0:
+        # grab the last index in the indexes list and add the index value
+        # to the list of picked indexes
+        last = len(idxs) - 1
+        i = idxs[last]
+        pick.append(i)
+
+        # find the largest (x, y) coordinates for the start of the bounding
+        # box and the smallest (x, y) coordinates for the end of the bounding
+        # box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        # compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+
+        # delete all indexes from the index list that have overlap greater
+        # than the provided overlap threshold
+        idxs = np.delete(idxs, np.concatenate(([last],
+                                               np.where(overlap > overlapthresh)[0])))
+
+    # return only the bounding boxes that were picked
+    return (pick, boxes[pick])
+
+
+def get_predictions(data):
+
+    url = "http://localhost:8501/v1/models/faster_rcnn:predict"
+    json_response = requests.post(url=url, data=data)
+    response = json.loads(json_response.text)
+    boxes = response['predictions'][0]['detection_boxes']
+    classes = response['predictions'][0]['detection_classes']
+    scores = response['predictions'][0]['detection_scores']
+    scores = np.squeeze(scores)
+    boxes = np.squeeze(boxes)
+    classes = np.squeeze(classes).astype(np.int32)
+    return [boxes, classes, scores]
+
+
+def post_process(boxes, classes, scores):
+    index = np.nonzero(scores)
+    scores = scores[index]
+    boxes = boxes[index]
+    classes = classes[index]
+    id, value = non_max_suppression(boxes, scores)
+    boxes = boxes[id]
+    classes = classes[id]
+    scores = scores[id]
+    return [boxes, classes, scores]
+
+
+def visualize(input, boxes, classes, scores):
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        input,
+        boxes,
+        classes,
+        scores,
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=8)
+
+
+from sagemaker.tensorflow.serving import Model,  Predictor
+from sagemaker.tensorflow import TensorFlowPredictor
+#model = Model(model_data='s3://my-sage-maker-test/faster_rcnn.tar.gz',
+#              role='arn:aws:iam::646335046581:role/service-role/AmazonSageMaker-ExecutionRole-20191125T144296',
+#              framework_version="1.12")
+#predictor = model.deploy(initial_instance_count=1,
+#                         instance_type='ml.p3.16xlarge')
+
 #exit(1)
-vis_util.visualize_boxes_and_labels_on_image_array(
-            input,
-            boxes,
-            classes,
-            scores,
-            category_index,
-            use_normalized_coordinates=True,
-            line_thickness=8)
-cv2.imshow('object detection', cv2.resize(input, (800, 600)))
-cv2.waitKey()
+image = 'test_image/test_4.jpg'
+endpoint = 'sagemaker-tensorflow-serving-2019-11-25-18-54-08-150'
+#input, data = prepare_request(image)
+with tf.gfile.GFile('test_image/test_3.jpg', 'rb') as fid:
+    encoded_jpg = base64.b64encode(fid.read())
+image_base64 = encoded_jpg.decode()
+data = {"instances": [{"image_bytes": {"b64": image_base64}}]}
+predictor = Predictor(endpoint)
+response = predictor.predict(data)
+print (response)
+
+
+
+exit(1)
+start_time = time.time()
+image = 'test_image/test_4.jpg'
+input, data = prepare_request(image)
+boxes, classes, scores = get_predictions(data)
+boxes, classes, scores = post_process(boxes, classes, scores)
+#visualize(input, boxes, classes, scores)
+#cv2.imshow('window', cv2.resize(input, (800, 600)))
+#cv2.waitKey()
+end_time = time.time()
+
+execution_time = end_time - start_time
+print (execution_time)
+
+
+
+# on my machine inference time = 26 seconds
+# train = 1.12 tf version
